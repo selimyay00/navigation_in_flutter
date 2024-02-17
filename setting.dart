8a960dc9cec35c56ec41710015b7ba3a7f0c18ff
Selim Yay <149972864+selimyay00@users.noreply.github.com>
2024-02-17 23:22:40 +0300

    Create setting.dart

---
+++ setting.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Go back to Home"),
+        ),
+      ),
+    );
+  }
+}
